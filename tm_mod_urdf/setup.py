56fb8f7be7bcbf21648d794a6173f844e39597fe
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2026-02-26 14:27:24 +0100

    (mod_urdf): get SN and hand cam from robot (#17)

--- tm_mod_urdf/setup.py
+++ tm_mod_urdf/setup.py
@@ -4,7 +4,7 @@ package_name = "tm_mod_urdf"
 
 setup(
     name=package_name,
-    version='2.0.0',
+    version="2.0.1",
     packages=[package_name],
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
@@ -16,12 +16,6 @@ setup(
     maintainer_email="ken.tsai@tm-robot.com",
     description="tm_mod_urdf",
     license="BSD-3-Clause",
-    extras_require={
-        "test": ["pytest"],
-    },
-    entry_points={
-        "console_scripts": [
-            "modify_xacro = tm_mod_urdf.modify_xacro:main",
-        ]
-    },
+    extras_require={"test": ["pytest"]},
+    entry_points={"console_scripts": ["modify_xacro = tm_mod_urdf.modify_xacro:main"]},
 )
